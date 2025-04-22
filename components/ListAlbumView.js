5bce5a9f9856ebd2cb4c968a85a44d0c5517e7ca
kokoraro <v.aurora0000@gmail.com>
2025-04-23 00:55:37 +0800

    Change List View

--- components/ListAlbumView.js
+++ components/ListAlbumView.js
@@ -1,15 +1,18 @@
-import Image from "next/image"
+import Image from "next/image";
+import Link from "next/link";
 
 const ListAlbumView = ({ title, image, date, songs }) => {
-    return (
-        <div className="px-2 py-3 flex gap-4 hover:shadow-[0px_0px_8px_4px_#02d95667] duration-500 cursor-pointer rounded-md">
-            <Image src={image.url} width={image.width} height={image.height} alt={title} className="w-[100px] aspect-square rounded-md" />
-            <div className="flex justify-center flex-col">
-                <h1 className="text-2xl">{title}</h1>
-                <h2 className="text-md">Album • {new Date(date).getFullYear()} • {songs} {songs < 2 ? "song" : 'songs'}</h2>
-            </div>
-        </div>
-    )
-}
+	return (
+		<Link className="px-2 py-3 flex gap-4 hover:shadow-[0px_0px_8px_4px_#02d95667] duration-500 cursor-pointer rounded-md" href={{ pathname: "/album/" + id }}>
+			<Image src={image.url} width={image.width} height={image.height} alt={title} className="w-[100px] aspect-square rounded-md" />
+			<div className="flex justify-center flex-col">
+				<h1 className="text-2xl">{title}</h1>
+				<h2 className="text-md">
+					Album • {new Date(date).getFullYear()} • {songs} {songs < 2 ? "song" : "songs"}
+				</h2>
+			</div>
+		</Link>
+	);
+};
 
-export default ListAlbumView
+export default ListAlbumView;
