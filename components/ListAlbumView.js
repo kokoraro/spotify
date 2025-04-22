3a94d37b8426a0fb284c04d3cc8e9fc24c83c59d
kokoraro <v.aurora0000@gmail.com>
2025-04-23 01:44:21 +0800

    ListView Modify

--- components/ListAlbumView.js
+++ components/ListAlbumView.js
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
 
-const ListAlbumView = ({ title, image, date, songs }) => {
+const ListAlbumView = ({ title, image, date, songs, id }) => {
 	return (
 		<Link className="px-2 py-3 flex gap-4 hover:shadow-[0px_0px_8px_4px_#02d95667] duration-500 cursor-pointer rounded-md" href={{ pathname: "/album/" + id }}>
 			<Image src={image.url} width={image.width} height={image.height} alt={title} className="w-[100px] aspect-square rounded-md" />
