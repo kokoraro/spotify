a7e9e838ee5563929a4659b246c27168802bae6e
kokoraro <v.aurora0000@gmail.com>
2025-04-24 08:13:07 +0800

    Add Console Backend

--- app/api/artists/route.js
+++ app/api/artists/route.js
@@ -1,11 +1,11 @@
 import spotifyApi from "../spotify"; // Import the shared SpotifyWebApi instance
 
 export async function GET(req) {
-    const dd = await spotifyApi.clientCredentialsGrant();
+	const dd = await spotifyApi.clientCredentialsGrant();
 	const token = dd.body["access_token"];
 	spotifyApi.setAccessToken(token);
 	const url = new URL(req.url);
-	const ids = url.searchParams.get("ids"); 
+	const ids = url.searchParams.get("ids");
 	if (!ids) {
 		return new Response(JSON.stringify({ error: "No artist IDs provided" }), {
 			status: 400,
@@ -15,10 +15,11 @@ export async function GET(req) {
 		});
 	}
 
-	const artistIds = ids.split(","); 
-	const data = await Promise.all(artistIds.map(id => spotifyApi.getArtist(id))); 
+	const artistIds = ids.split(",");
+	console.log(artistIds);
+	const data = await Promise.all(artistIds.map((id) => spotifyApi.getArtist(id)));
 
-	return new Response(JSON.stringify({ data: data.map(d => d.body) }), {
+	return new Response(JSON.stringify({ data: data.map((d) => d.body) }), {
 		status: 200,
 		headers: {
 			"Content-Type": "application/json",
