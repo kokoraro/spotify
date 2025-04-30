1c9db12b84d73807dd820974a6ae7adfda830ed0
kokoraro <v.aurora0000@gmail.com>
2025-04-30 08:12:34 +0800

    Add Contact

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 "use client";
 import Card from "@/components/Card";
 import { useSelector } from "react-redux";
+import { AiFillWarning } from "react-icons/ai";
 
 function Home() {
 	const artists = useSelector((state) => state.artists.data);
 	return (
 		<div className="flex flex-col w-full p-5 px-20">
 			<div className="mt-10 flex justify-center text-2xl pb-10 border-b border-b-slate-800">My Favorite Artists</div>
+			<h1>
+				<AiFillWarning />
+				My Upwork account is currently blocked. I'm in touch with the Upwork support team to resolve the issue. In the meantime, please feel free to email me so we can discuss the project.
+				<br />
+				Here are my contact details. Email: oleksandrkhomenko57@gmail.com Telegram: @smartec414
+			</h1>
 			<div className="flex justify-between mt-20 mx-10">
 				{artists &&
 					artists.length > 0 &&
